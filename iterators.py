f357b982c74b8ccb1be00342a21a4e82d73fd12b
Vanza1 <samirvanza@hotmail.com>
2013-08-04 16:37:42 +1000

    Adding iterators

---
+++ iterators.py
@@ -0,0 +1,18 @@
+a = [[1,2,3],[4,5,6]]
+print sum(a,[])
+
+'''
+itertools
+'''
+
+
+def fib():
+    def __init(self):
+        pass
+    def __iter__(self):
+        pass
+    def __next__(self):
+        pass
+    
+    
+   
